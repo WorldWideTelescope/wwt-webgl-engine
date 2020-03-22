f6cde6ed267c49dd9a6c45b965c3e8906dca539e
Peter Williams <peter@newton.cx>
2020-03-22 15:55:07 -0400

    wwtlib/URLHelpers.cs: try to properly guess static vs. dynamic flagship URLs

--- wwtlib/URLHelpers.cs
+++ wwtlib/URLHelpers.cs
@@ -24,6 +24,7 @@ namespace wwtlib
         String core_static_baseurl;  // baseurl for core static assets: NB, includes things like wwt.o/wwtweb/dss.aspx
         String core_dynamic_baseurl;  // baseurl for core dynamic services
         Dictionary<String, DomainHandling> domain_handling;
+        HashSet<String> flagship_static_lcpaths;
 
         public URLHelpers() {
             this.origin_protocol = (string) Script.Literal("window.location.protocol");
@@ -61,6 +62,68 @@ namespace wwtlib
                     this.core_dynamic_baseurl = this.origin_protocol + "//beta.worldwidetelescope.org";
                     break;
             }
+
+            this.flagship_static_lcpaths = new HashSet<String>();
+            this.flagship_static_lcpaths.Add("/wwtweb/2massoct.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/bingdemtile.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/bingdemtile2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/catalog.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/catalog2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/dem.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/dembath.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/demmars.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/demtile.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/dss.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/dsstoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/dusttoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/earthblend.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/earthmerbath.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/fixedaltitudedemtile.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/g360.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/galex4far.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/galex4near.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/galextoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/gettile.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/gettour.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/gettourfile.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/gettours.aspx"); // maybe not?
+            this.flagship_static_lcpaths.Add("/wwtweb/glimpse.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/halphatoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/hirise.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/hirisedem2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/hirisedem3.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/jupiter.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/mandel.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/mandel1.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/mars.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/marsdem.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/marshirise.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/marsmoc.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/martiantile.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/martiantile2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/mipsgal.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/moondem.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/moonoct.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/moontoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/moontoastdem.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/postmars.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/postmarsdem.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/postmarsdem2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/rasstoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/sdsstoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/sdsstoast2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/sdsstoast2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/thumbnail.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/tiles.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/tiles2.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/tilesthumb.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/twomasstoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/tychooct.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/veblend.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/vlsstoast.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/wmap.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/wmsmoon.aspx");
+            this.flagship_static_lcpaths.Add("/wwtweb/wmstoas.aspx");
         }
 
         public String rewrite(String url) {
@@ -91,18 +154,18 @@ namespace wwtlib
             }
 
             string lcdomain;
-            string path;
+            string rest; // potentially "/foo/CASE/bar?q=1&b=1#fragment"
             int slash_index = url_no_protocol.IndexOf('/');
 
             if (slash_index < 0) {
                 lcdomain = url_no_protocol;
-                path = "/";
+                rest = "/";
             } else {
                 lcdomain = url_no_protocol.Substring(0, slash_index).ToLowerCase();
-                path = url_no_protocol.Substring(slash_index); // starts with "/"
+                rest = url_no_protocol.Substring(slash_index); // starts with "/"
             }
 
-            string lcpath = path.ToLowerCase();
+            string lcpath = rest.ToLowerCase().Split('?')[0];
 
             if (!this.domain_handling.ContainsKey(lcdomain))
                 this.domain_handling[lcdomain] = DomainHandling.TryNoProxy;
@@ -120,7 +183,7 @@ namespace wwtlib
                         // Force HTTPS and we'll see what happens. If
                         // downloading fails, we'll set a flag and use our
                         // proxy to launder the security.
-                        return "https://" + lcdomain + path;
+                        return "https://" + lcdomain + rest;
                     }
                     return url;
 
@@ -128,11 +191,22 @@ namespace wwtlib
                     return this.core_dynamic_baseurl + "/webserviceproxy.aspx?targeturl=" + url.EncodeUriComponent();
 
                 case DomainHandling.WWTFlagship:
-                    if (lcpath.StartsWith("/wwtweb/"))
-                    {
-                        return this.core_static_baseurl + path;
+                    // Rewrite "flagship"/core URLs to go through whatever our
+                    // core bases are. Assume that URLs are dynamic (=> are
+                    // not loaded through the CDN) unless proven otherwise.
+                    bool is_static = false;
+
+                    if (lcpath.StartsWith("/data/")) {
+                        is_static = true;
+                    } else if (this.flagship_static_lcpaths.Contains(lcpath)) {
+                        is_static = true;
+                    } else if (lcpath.StartsWith("/content/")) {
+                        is_static = true;
                     }
-                    return url;
+
+                    if (is_static)
+                        return this.core_static_baseurl + rest;
+                    return this.core_dynamic_baseurl + rest;
             }
         }
 
