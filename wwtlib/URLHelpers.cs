947ad438083ea9fcbe3cffdeeab87578c04231bb
Peter Williams <peter@newton.cx>
2020-03-21 23:58:28 -0400

    wwtlib/URLHelpers.cs: stub a helper class for URL rewriting
    This is gonna be messy ...

---
+++ wwtlib/URLHelpers.cs
@@ -0,0 +1,28 @@
+﻿// The helper class for rewriting URLs. This gets complicated, because we
+// might need to proxy for CORS headers and/or HTTPS support, *and* we
+// sometimes also want to change the host and/or path to allow the engine or
+// the webclient to swap out the data backend or the frontend.
+
+using System;
+
+namespace wwtlib
+{
+    public class URLHelpers
+    {
+        String origin_protocol;
+
+        public URLHelpers() {
+            this.origin_protocol = (string) Script.Literal("window.location.protocol");
+        }
+
+        public String rewrite(String url) {
+            return url;
+        }
+
+        public static URLHelpers singleton;
+
+        static URLHelpers() {
+            singleton = new URLHelpers();
+        }
+    }
+}
