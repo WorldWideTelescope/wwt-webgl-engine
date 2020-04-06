940fa4cbab58f59db4788cac289f78249c1439d1
Peter Williams <peter@newton.cx>
2020-04-06 16:58:51 -0400

    wwtlib/WWTControl.cs: typo fix

--- wwtlib/WWTControl.cs
+++ wwtlib/WWTControl.cs
@@ -2778,7 +2778,7 @@ namespace wwtlib
             }
         }
 
-        private SimpleLineList crossHarirs = null;
+        private SimpleLineList crossHairs = null;
 
         private void DrawCrosshairs(RenderContext context)
         {
@@ -2804,15 +2804,15 @@ namespace wwtlib
             }
             else
             {
-                if (crossHarirs == null)
+                if (crossHairs == null)
                 {
-                    crossHarirs = new SimpleLineList();
-                    crossHarirs.DepthBuffered = false;
-                    crossHarirs.Pure2D = true;
-                    crossHarirs.AddLine(Vector3d.Create(-.02, .0, 0), Vector3d.Create(.02, 0, 0));
-                    crossHarirs.AddLine(Vector3d.Create(0,-.03, 0), Vector3d.Create(0, .03, 0));
+                    crossHairs = new SimpleLineList();
+                    crossHairs.DepthBuffered = false;
+                    crossHairs.Pure2D = true;
+                    crossHairs.AddLine(Vector3d.Create(-.02, .0, 0), Vector3d.Create(.02, 0, 0));
+                    crossHairs.AddLine(Vector3d.Create(0,-.03, 0), Vector3d.Create(0, .03, 0));
                 }
-                crossHarirs.DrawLines(context, 1.0f, Colors.White);
+                crossHairs.DrawLines(context, 1.0f, Colors.White);
             }
         }
 
