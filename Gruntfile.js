99d388e8eae7478d465b9e298ada5fe59e08b2a1
Peter Williams <peter@newton.cx>
2020-04-20 16:45:47 -0400

    First cut at a modularized/TypeScriptified 7.x series

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,8 @@
 module.exports = function (grunt) {
   'use strict';
 
-  // Force use of Unix newlines
-  grunt.util.linefeed = '\n';
-
-  RegExp.quote = function (string) {
-    return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
-  };
-
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    //remove the AMD dependency from the ScriptSharp output.
-    replace: {
-      wwtlib: {
-        src: ['wwtlib/bin/wwtlib.js'],
-        dest: 'wwtlib-replaced.js',
-        replacements: [
-          {
-            from: "define('wwtlib', ['ss'], function(ss) {",
-            to: 'window.wwtlib = function(){'
-          },
-          {
-            from: 'return $exports;\n});',
-            to: 'return $exports;\n}();'
-          }
-        ]
-      }
-    },
 
     concat: {
       options: {
@@ -38,21 +12,14 @@ module.exports = function (grunt) {
       sdk: {
         src: [
           'scriptsharp-0.8.0/ss.js',
-          '<%= replace.wwtlib.dest %>'
+          'wwtlib/bin/wwtlib.js'
         ],
         dest: 'wwtsdk.js'
-      },
-    },
-
-    uglify: {
-      sdk: {
-        src: '<%= concat.sdk.dest %>',
-        dest: 'wwtsdk.min.js'
       },
     }
   });
 
   require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
 
-  grunt.registerTask('dist-js', ['replace:wwtlib', 'concat:sdk', 'uglify:sdk']);
+  grunt.registerTask('dist-js', ['concat:sdk']);
 };
