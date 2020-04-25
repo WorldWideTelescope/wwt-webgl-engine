f132e5f36397490dd454477feb830e2c5a59e41c
Peter Williams <peter@newton.cx>
2020-04-25 10:45:24 -0400

    src/index.d.ts: document SpaceTimeController time functions

--- src/index.d.ts
+++ src/index.d.ts
@@ -15,6 +15,102 @@ export class ScriptInterface {
   getDec(): number;
 }
 
+export namespace SpaceTimeController {
+  /** Have the WWT clock immediately lock onto to the system clock.
+   *
+   * This function not only has the effect of calling [[set_syncToClock]] with a
+   * true argument, it also sets the offset between the two clocks to be zero.
+   */
+  export function syncTime(): void;
+
+  /** Get the current time as a JavaScript Date in the UTC timescale. */
+  export function get_now(): Date;
+
+  /** Set the current time as a JavaScript Date in the UTC timescale.
+   *
+   * @param date The date.
+   * @returns The date that was just set.
+   */
+  export function set_now(date: Date): Date;
+
+  /** Get whether the WWT clock moves at the same rate as the system clock.
+   *
+   * Note that this value may be true but there may still be a constant offset
+   * between the two clocks.
+   */
+  export function get_syncToClock(): boolean;
+
+  /** Set whether the WWT clock moves at the same rate as the system clock.
+   *
+   * @param sync Whether the clock rates are to be synchronized.
+   * @returns The input argument.
+   *
+   * If set to false, the WWT clock will stop advancing, ignoring the value of
+   * [[get_timeRate]]. If set to true, the WWT clock will resume advancing from
+   * where it left off, possibly inducing an offset between the WWT clock and
+   * the system clock.
+   */
+  export function set_syncToClock(sync: boolean): boolean;
+
+  /** Get the rate at which the WWT clock advances relative to the system time.
+   *
+   * A value of 1 means that the two clocks stay in sync. A value of 10 means
+   * that time in WWT proceeds 10 times faster than system time. The rate may be
+   * negative.
+   */
+  export function get_timeRate(): number;
+
+  /** Set the rate at which the WWT clock advances relative to the system time.
+   *
+   * @param rate The rate factor.
+   * @returns The input argument.
+   *
+   * A value of 1 means that the two clocks stay in sync. A value of 10 means
+   * that time in WWT proceeds 10 times faster than system time. The rate may be
+   * negative.
+   *
+   * Do not set the rate to zero. Instead, call [[set_syncToClock]] with a false
+   * argument.
+   */
+  export function set_timeRate(rate: number): number;
+
+  /** Get the current time as a Julian date in the UTC timescale (which is
+   * problematic)
+   *
+   * Julian dates measure time as a continuous number of days since a reference
+   * point far in the past. Dates around the present will be numbers larger than
+   * 2.4 million.
+   *
+   * Note that it is not rigorously correct to express UTC times as Julian
+   * dates, because it is not well-defined how to handle leap seconds. As such,
+   * the accuracy of time-dependent computations in WWT should only be
+   * trusted to granularities of about a minute.
+   */
+  export function get_jNow(): number;
+
+  /** Convert a Julian date to a JavaScript UTC datetime.
+   *
+   * @param jdate The Julian date, usually a number around 2.4 million.
+   * @returns A Javascript datetime.
+   *
+   * See [[get_jNow]] for commentary on this conversion, which is not rigorously
+   * correct and can only be trusted to granularities of less than around a
+   * minute.
+   */
+  export function julianToUtc(jdate: number): Date;
+
+  /** Convert a JavaScript UTC datetime to a Julian date.
+   *
+   * @param date The datetime.
+   * @returns A Julian date.
+   *
+   * See [[get_jNow]] for commentary on this conversion, which is not rigorously
+   * correct and can only be trusted to granularities of less than around a
+   * minute.
+   */
+  export function utcToJulian(date: Date): number;
+}
+
 export class WWTControl {
   /** Render the view.
    *
