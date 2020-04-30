e4a5972e4b89beac256a92da402a4767f6620e07
Peter Williams <peter@newton.cx>
2020-04-29 22:04:08 -0400

    .eslintrc.js: add this
    Seems that some package consumers might want it to be there.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
