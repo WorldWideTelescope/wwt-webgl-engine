d711213d0a1ba3037db32ac6bff6397044593660
Peter Williams <peter@newton.cx>
2020-05-03 11:06:05 -0400

    build(semantic-release-ado-monorepo): output a newMajMin variable as well

--- semantic-release-ado-monorepo/src/index.js
+++ semantic-release-ado-monorepo/src/index.js
@@ -3,9 +3,15 @@
 
 const prepare = async (_pluginConfig, { nextRelease: { version }, logger }) => {
   const packageSanitized = process.env.LERNA_PACKAGE_NAME.replace(/[^a-zA-Z0-9_]/g, '_');
-  const varName = `newVersion${packageSanitized}`;
-  logger.log(`Setting variable ${varName} to ${version}`);
-  console.log(`##vso[task.setvariable variable=${varName}]${version}`);
+
+  const fullVersionVar = `newVersion${packageSanitized}`;
+  logger.log(`Setting variable ${fullVersionVar} to ${version}`);
+  console.log(`##vso[task.setvariable variable=${fullVersionVar}]${version}`);
+
+  const majMin = version.split('.').slice(0, 2).join('.');
+  const majMinVar = `newMajMin${packageSanitized}`;
+  logger.log(`Setting variable ${majMinVar} to ${majMin}`);
+  console.log(`##vso[task.setvariable variable=${majMinVar}]${majMin}`);
 };
 
 module.exports = {
