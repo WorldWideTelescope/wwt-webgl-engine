11aa39235676d8cc45674907ddad4f685f884727
Henrik Norman <honke.norman@gmail.com>
2020-10-18 15:22:57 +0200

    Adapted Hploc for ScriptSharp

--- engine/wwtlib/Hploc.cs
+++ engine/wwtlib/Hploc.cs
@@ -4,56 +4,82 @@ namespace wwtlib
 {
     public class Hploc
     {
-        public double z, phi, sth;
+        public double z;
+        public double phi;
+        public double sth;
         public bool have_sth;
 
         /** Default constructor. */
         public Hploc() { }
-        public Hploc(Vector3d v)
+        public static Hploc Create(Vector3d v)
         {
+            Hploc temp = new Hploc();
+
             double xl = 1 / v.Length();
-            z = v.Z * xl;
-            phi = FastMath.atan2(v.Y, v.X);
-            if (Math.Abs(z) > 0.99)
+            temp.z = v.Z * xl;
+            temp.phi = FastMath.atan2(v.Y, v.X);
+            if (Math.Abs(temp.z) > 0.99)
             {
-                sth = Math.Sqrt(v.X * v.X + v.Y * v.Y) * xl;
-                have_sth = true;
+                temp.sth = Math.Sqrt(v.X * v.X + v.Y * v.Y) * xl;
+                temp.have_sth = true;
             }
+            return temp;
         }
-        public Hploc(Zphi zphi)
-        {
-            z = zphi.z;
-            phi = zphi.phi;
-            have_sth = false;
-        }
-        public Hploc(Pointing ptg)
-        {
-            HealpixUtils.check((ptg.theta >= 0d) && (ptg.theta <= Math.PI),
-      "invalid theta value");
-            z = FastMath.cos(ptg.theta);
-            phi = ptg.phi;
-            if (Math.Abs(z) > 0.99)
-            {
-                sth = FastMath.sin(ptg.theta);
-                have_sth = true;
-            }
-        }
+      //  public static Hploc Create(Zphi zphi)
+      //  {
+      //      Hploc temp = new Hploc();
+      //      temp.z = zphi.z;
+      //      temp.phi = zphi.phi;
+      //      temp.have_sth = false;
+      //      return temp;
+      //  }
+      //  public static Hploc Create(Pointing ptg)
+      //  {
+      //      Hploc temp = new Hploc();
+      //      HealpixUtils.check((ptg.theta >= 0d) && (ptg.theta <= Math.PI),
+      //"invalid theta value");
+      //      temp.z = FastMath.cos(ptg.theta);
+      //      temp.phi = ptg.phi;
+      //      if (Math.Abs(temp.z) > 0.99)
+      //      {
+      //          temp.sth = FastMath.sin(ptg.theta);
+      //          temp.have_sth = true;
+      //      }
+      //      return temp;
+      //  }
 
         public Zphi toZphi()
-        { return new Zphi(z, phi); }
+        { return Zphi.Create(z, phi); }
 
         public Pointing toPointing()
         {
-            double st = have_sth ? sth : Math.Sqrt((1.0 - z) * (1.0 + z));
-            return new Pointing(FastMath.atan2(st, z), phi);
+            double st;
+            if (have_sth)
+            {
+                st = sth;
+            } else
+            {
+                st = Math.Sqrt((1.0 - z)  * (1.0 + z));
+            }
+
+            return Pointing.Create(FastMath.atan2(st, z), phi);
         }
 
         public Vector3d toVec3()
         {
-            double st = have_sth ? sth : Math.Sqrt((1.0 - z) * (1.0 + z));
+            double st;
+            if (have_sth)
+            {
+                st = sth;
+            }
+            else
+            {
+                st = Math.Sqrt((1.0 - z) * (1.0 + z));
+            }
+
             double x = st * FastMath.cos(phi);
             double y = st * FastMath.sin(phi);
-            return new Vector3d(-x, z, -y);
+            return Vector3d.Create(-x, z, -y);
             //return new Vector3d(x, z, y); //for planet
 
             //switch (Earth3d.DebugValue)
