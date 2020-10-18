0014ad8f02b849fb48a9068daf1eba337787ce31
Henrik Norman <honke.norman@gmail.com>
2020-10-18 15:22:15 +0200

    Adapted HealpixUtils for ScriptSharp

--- engine/wwtlib/HealpixUtils.cs
+++ engine/wwtlib/HealpixUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace wwtlib
 {
     public class HealpixUtils
@@ -10,12 +11,35 @@ namespace wwtlib
 
         /** Integer base 2 logarithm.
          */
+        //static public int ilog2(long arg)
+        //{
+        //    int res = (int)Math.Log(arg, 2);
+
+        //    return res;
+        //}
+
+
         static public int ilog2(long arg)
         {
-            int res = (int)Math.Log(arg, 2);
-
-            return res;
+            //	return Math.max(Math.log2(arg));
+            return 63 - HealpixUtils.numberOfLeadingZeros(Math.Max(arg, 1));
         }
+
+        static public int numberOfLeadingZeros(int i)
+        {
+            if (i == 0)
+                return 64;
+            int n = 1;
+            int x = (i >> 32);
+            if (x == 0) { n += 32; x = i; }
+            if (x >> 16 == 0) { n += 16; x <<= 16; }
+            if (x >> 24 == 0) { n += 8; x <<= 8; }
+            if (x >> 28 == 0) { n += 4; x <<= 4; }
+            if (x >> 30 == 0) { n += 2; x <<= 2; }
+            n -= x >> 31;
+            return n;
+        }
+
 
         /** Integer square root.
          */
@@ -39,8 +63,8 @@ namespace wwtlib
         }
         /** Computes the cosine of the angular distance between two z, phi positions
             on the unit sphere. */
-        static public double cosdist_zphi(Zphi zp1, Zphi zp2)
-        { return cosdist_zphi(zp1.z, zp1.phi, zp2.z, zp2.phi); }
+        //static public double cosdist_zphi(Zphi zp1, Zphi zp2)
+        //{ return cosdist_zphi(zp1.z, zp1.phi, zp2.z, zp2.phi); }
 
 
         static public double fmodulo(double v1, double v2)
@@ -51,9 +75,9 @@ namespace wwtlib
             return (tmp == v2) ? 0d : tmp;
         }
 
-        static public bool approx(float a, float b, float epsilon)
-        { return Math.Abs(a - b) < (epsilon * Math.Abs(b)); }
-        static public bool approx(double a, double b, double epsilon)
-        { return Math.Abs(a - b) < (epsilon * Math.Abs(b)); }
+        //static public bool approx(float a, float b, float epsilon)
+        //{ return Math.Abs(a - b) < (epsilon * Math.Abs(b)); }
+        //static public bool approx(double a, double b, double epsilon)
+        //{ return Math.Abs(a - b) < (epsilon * Math.Abs(b)); }
     }
 }
