aaeccd693524f015697dbdfa62e11dcc3048b012
Henrik Norman <honke.norman@gmail.com>
2020-10-18 16:03:52 +0200

    Added Healpix ProjectionType

--- engine/wwtlib/IImageSet.cs
+++ engine/wwtlib/IImageSet.cs
@@ -42,7 +42,7 @@ namespace wwtlib
     //    string ReferenceFrame { get; set; }
     //}
 
-    public enum ProjectionType { Mercator = 0, Equirectangular = 1, Tangent = 2, Tan = 2, Toast = 3, Spherical = 4, SkyImage = 5, Plotted = 6 };
+    public enum ProjectionType { Mercator = 0, Equirectangular = 1, Tangent = 2, Tan = 2, Toast = 3, Spherical = 4, SkyImage = 5, Plotted = 6, Healpix = 7 };
     public enum ImageSetType { Earth = 0, Planet = 1, Sky = 2, Panorama = 3, SolarSystem = 4, Sandbox = 5};
     public enum BandPass { Gamma = 0, XRay = 1, Ultraviolet = 2, Visible = 3, HydrogenAlpha = 4, IR = 4, Microwave = 5, Radio = 6, VisibleNight = 6 };
 }
