1334ada403310494c963e81dac098fe7f32c7ffb
Henrik Norman <honke.norman@gmail.com>
2021-06-20 11:28:00 +0200

    Posting image_collection_loaded event on folder load complete

--- research-app/src/App.vue
+++ research-app/src/App.vue
@@ -706,7 +706,9 @@ export default class App extends WWTAwareComponent {
 
   onMessage(msg: any) {  // eslint-disable-line @typescript-eslint/no-explicit-any
     if (classicPywwt.isLoadImageCollectionMessage(msg)) {
-      this.loadImageCollection({ url: msg.url, loadChildFolders: msg.loadChildFolders });
+      this.loadImageCollection({ url: msg.url, loadChildFolders: msg.loadChildFolders }).then(()=> {
+        this.statusMessageDestination.postMessage({event: "image_collection_loaded", url: msg.url}, this.allowedOrigin);
+      });
     } else if (classicPywwt.isSetBackgroundByNameMessage(msg)) {
       this.setBackgroundImageByName(msg.name);
     } else if (classicPywwt.isSetForegroundByNameMessage(msg)) {
