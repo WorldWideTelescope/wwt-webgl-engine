2c604c93b86a9db26263b785c290049967d5e999
Carifio24 <carifio24@gmail.com>
2022-02-17 14:35:34 -0500

    Add check to avoid importing pako when it's already present from a CDN. Modify assignment function in promise to not be an arrow function.

--- engine/wwtlib/Imports.cs
+++ engine/wwtlib/Imports.cs
@@ -3,9 +3,17 @@ using System;
 public static class Imports
 {
 
+    /* It would ideal to use a helper function to generate the string literals here
+       given the library name, but Script.Literal requires a constant string.
+       It's not possible to create a constant string via interpolation until C# 10.
+    */
     static Imports()
     {
-        Script.Literal("let pako = null");
-        Script.Literal("import('pako').then(result => { pako = result; })");
+        Script.Literal("let pako");
+        Script.Literal("if (typeof window !== \"undefined\" && \"pako\" in window) {");
+        Script.Literal("  pako = window[\"pako\"]");
+        Script.Literal("} else {");
+        Script.Literal("  import('pako').then(function(result) { pako = result; })");
+        Script.Literal("}");
     }
 }
