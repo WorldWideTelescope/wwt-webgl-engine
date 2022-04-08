c9018efa89f91f45ff43e3e170069628d67dd5d3
Carifio24 <carifio24@gmail.com>
2022-04-08 11:52:23 -0400

    Add the new tour messages file.

---
+++ research-app-messages/src/tours.ts
@@ -0,0 +1,32 @@
+// Copyright 2021 the .NET Foundation
+// Licensed under the MIT License
+
+/** Messages relating to tours.
+ *
+ * This module contains messages and types relating to WWT tours,
+ *  in particular obtaining the current view as a tour.
+ *  However, note that the [[classicPywwt]] module defines a bunch
+ *  of messages on the same topic, especially concerning
+ *  loading and playback of tours.
+ *
+ * The defined messages are:
+ *
+ * - [[GetViewAsTourMessage]] leading to [[GetViewAsTourReply]]
+ *
+ * */
+
+export interface GetViewAsTourMessage {
+  type: "get_view_as_tour",
+  threadId: string;
+}
+
+export function isGetViewAsTourMessage(o: any): o is GetViewAsTourMessage {
+  return o.type === "get_view_as_tour" &&
+    typeof o.threadId === "string";
+}
+
+export interface GetViewAsTourReply {
+  type: "get_view_as_tour_reply",
+  threadId: string;
+  tourXml: string | null;
+}
