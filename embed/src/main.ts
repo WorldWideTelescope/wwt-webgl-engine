7256fcc992cead09413d82588c8e4c8f9c19e217
Carifio24 <carifio24@gmail.com>
2022-10-07 11:14:31 -0400

    Update plumbing for embed.

--- embed/src/main.ts
+++ embed/src/main.ts
@@ -1,6 +1,5 @@
-import Vue from "vue";
-import VTooltip from "v-tooltip";
-import Vuex from "vuex";
+import { createApp } from "vue";
+import FloatingVue from "floating-vue";
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
@@ -21,22 +20,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import VueSlider from 'vue-slider-component';
 import 'vue-slider-component/theme/default.css';
 
-import { createPlugin } from "@wwtelescope/engine-vuex";
 import { EmbedSettings } from "@wwtelescope/embed-common";
 
 import Embed from "./Embed.vue";
-
-Vue.config.productionTip = false;
-
-Vue.use(VTooltip);
-Vue.use(Vuex);
-
-const store = new Vuex.Store({});
-
-Vue.use(createPlugin(), {
-  store,
-  namespace: "wwt-embed"
-});
 
 library.add(faAdjust);
 library.add(faCompress);
@@ -49,22 +35,14 @@ library.add(faSearchMinus);
 library.add(faSearchPlus);
 library.add(faSlidersH);
 library.add(faUndoAlt);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-Vue.component('vue-slider', VueSlider);
 
 const queryParams = new URLSearchParams(window.location.search);
 const settings = EmbedSettings.fromQueryParams(queryParams.entries());
 
-new Vue({
-  store,
-  el: "#app",
-  render: createElement => {
-    return createElement(Embed, {
-      props: {
-        "wwtNamespace": "wwt-embed",
-        "embedSettings": settings
-      }
-    });
-  }
-});
+createApp(Embed)
+  .use(FloatingVue)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .component('vue-slider', VueSlider)
+  .provide('wwtNamespace', 'wwt-embed')
+  .provide('embedSettings', settings)
+  .mount("#app");
