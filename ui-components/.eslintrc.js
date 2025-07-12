6bf93b7bcf12ed14158171d82a73d73d13e98262
Carifio24 <carifio24@gmail.com>
2025-07-12 12:11:09 -0400

    Finish Skyball component implementation.

--- ui-components/.eslintrc.js
+++ ui-components/.eslintrc.js
@@ -3,5 +3,13 @@ module.exports = {
     "../.eslintrc.js",
     "plugin:vue/essential",
     "@vue/typescript/recommended"
-  ]
+  ],
+  overrides: [
+    {
+      files: ['src/Skyball.vue'],
+      rules: {
+        'vue/multi-word-component-names': 'off',
+      },
+    },
+  ],
 };
