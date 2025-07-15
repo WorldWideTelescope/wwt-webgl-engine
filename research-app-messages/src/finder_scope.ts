6a7ca5c1cdff72d873e7b9d3fb8a425f58d51184
Carifio24 <carifio24@gmail.com>
2025-07-15 14:30:16 -0400

    Add additional comment for new message type.

--- research-app-messages/src/finder_scope.ts
+++ research-app-messages/src/finder_scope.ts
@@ -1,5 +1,16 @@
+// Copyright 2021-2023 the .NET Foundation
+// Licensed under the MIT License
+
 // Note: module-level doctring found in `index.ts`
 
+
+/** Information about the current state of the focused `Place` in the research app's
+* Finder Scope.
+*
+* If the scope's focused `Place` changes to be empty (including when the scope is closed),
+* the value of the `placeXml` field will be null. The XML serialization of the `Place` uses
+* the standard serialization employed elsewhere by the web engine.
+*/
 export interface FinderScopePlaceMessage {
   /** The tag identifying the message type */
   type: "finder_scope_place";
